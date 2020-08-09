97acfadc650b099e4ec7ccb7363f22e83e7f5ec5
hari-k6 <ha0265378@gmail.com>
2020-08-09 12:38:55 +0530

    firebase changes

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,9 +1,19 @@
 import * as functions from 'firebase-functions';
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as express from 'express'
+import * as bodyParser from 'body-parser';
+// import * as http from 'http';
+// import * as socket from 'socket.io';
+// import cors from 'cors';
+import userapi from './handlers/user/user.route';
+import roomapi from './handlers/room/room.route';
+// const { chat } = require('./handlers/chatserver/chatserver');
+const app: express.Application = express();
+// const server = http.createServer(app);
+// const io = socket.listen(server)
+app.use(bodyParser.json({}));
+// app.use(cors());
+app.use(express.static('public'));
+app.use('/user', userapi);
+app.use('/room', roomapi);
+// chat(io);
+export const firebaseserver = functions.https.onRequest(app)
